46ff5c8d11e8d6f778893b48d3afd55860c4b92d
pelayoalv <alvarezrodriguezpelayo@gmail.com>
2026-03-03 11:49:10 +0100

    Solucionado problema vista editar

--- src/app/loan/loan-edit/loan-edit.ts
+++ src/app/loan/loan-edit/loan-edit.ts
@@ -6,7 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { Client } from '../../client/model/Client';
 import { Game } from '../../game/model/game';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { LOANS_DATA } from '../model/mock-loan';
 import { ClientService } from '../../client/client.service';
 import { GameService } from '../../game/game.service';
@@ -27,21 +27,27 @@ import { Loan } from '../model/Loan';
   styleUrl: './loan-edit.scss',
 })
 export class LoanEdit implements OnInit {
-  client: Client;
   loan: Loan;
-  game: Game;
+  clients: Client[] = []; // Array para el desplegable de clientes
+  games: Game[] = [];     // Array para el desplegable de juegos
   errorMessage: string | null = null;
 
   constructor(
     public dialogRef: MatDialogRef<LoanEdit>,
-    @Inject(LOANS_DATA) public data: {loan: Loan},
+    @Inject(MAT_DIALOG_DATA) public data: {loan: Loan},
     private clientService: ClientService,
     private gameService: GameService,
     private loanService: LoanService
   ) { }
 
   ngOnInit(): void {
+    // this.loan = this.data.loan ? Object.assign({}, this.data.loan) : new Loan();
+    // Si pasamos un loan desde la tabla (para editar), lo clonamos. Si no, creamos uno nuevo vacío.
     this.loan = this.data.loan ? Object.assign({}, this.data.loan) : new Loan();
+
+    // Cargamos los datos para llenar los <mat-select>
+    this.clientService.getClients().subscribe(clients => this.clients = clients);
+    this.gameService.getGames().subscribe(games => this.games = games);
   }
 
   onSave() {
